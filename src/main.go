d1d0802f4c868e0f7fedae3368b3f24e4114e9df
Liloche19 <lilian.locher@epitech.eu>
2025-02-07 15:02:42 +0100

    [+] multithreading added

--- src/main.go
+++ src/main.go
@@ -4,63 +4,86 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sync"
 )
 
 func allocate(size int) [][]bool {
-	plate := make([][]bool, size)
+	table := make([][]bool, size)
 
 	for i := 0; i < size; i++ {
 
-		plate[i] = make([]bool, size)
+		table[i] = make([]bool, size)
 		for j := 0; j < size; j++ {
-			plate[i][j] = false
+			table[i][j] = false
 		}
 	}
-	return plate
+	return table
 }
 
-func is_safe(plate [][]bool, i int, j int, size int) bool {
+func is_safe(table [][]bool, i int, j int, size int) bool {
 	for k := 0; k < size; k++ {
-		if plate[k][j] && k != i {
+		if table[k][j] && k != i {
 			return false
 		}
 	}
 	for k := 0; k < size; k++ {
-		if plate[i][k] && k != j {
+		if table[i][k] && k != j {
 			return false
 		}
 	}
 	for k := 1; k < size; k++ {
-		if i - k >= 0 && j - k >= 0 && plate[i - k][j - k] {
+		if i - k >= 0 && j - k >= 0 && table[i - k][j - k] {
 			return false
 		}
-		if i + k < size && j - k >= 0 && plate[i + k][j - k] {
+		if i + k < size && j - k >= 0 && table[i + k][j - k] {
 			return false
 		}
-		if i + k < size && j + k < size && plate[i + k][j + k] {
+		if i + k < size && j + k < size && table[i + k][j + k] {
 			return false
 		}
-		if i - k >= 0 && j + k < size && plate[i - k][j + k] {
+		if i - k >= 0 && j + k < size && table[i - k][j + k] {
 			return false
 		}
 	}
 	return true
 }
 
-func nqueen(size int, plate [][]bool, step int) int {
-	possibilities := 0
+func nqueen(size int, table [][]bool, step int, wgprec *sync.WaitGroup, possibilities *int, col *int) int {
+    if step == size {
+        if possibilities != nil {
+            *possibilities++
+        }
+        return 1
+    }
+    var total int
+    if step == 0 {
+        var wg sync.WaitGroup
+        table = allocate(size)
+        for i := 0; i < size; i++ {
+            newTable := allocate(size)
+            newTable[0][i] = true
+            if is_safe(newTable, 0, i, size) {
+                wg.Add(1)
+                go func(col int) {
+                    defer wg.Done()
+                    localPoss := 0
+                    nqueen(size, newTable, 1, nil, &localPoss, &col)
+                    total += localPoss
+                }(i)
+            }
+        }
+        wg.Wait()
+        return total
+    }
+    for i := 0; i < size; i++ {
+        table[step][i] = true
+        if is_safe(table, step, i, size) {
+            total += nqueen(size, table, step+1, wgprec, possibilities, col)
+        }
+        table[step][i] = false
+    }
 
-	if step == size {
-		return 1
-	}
-	for i := 0; i < size; i++ {
-		plate[step][i] = true
-		if is_safe(plate, step, i, size) {
-			possibilities += nqueen(size, plate, step + 1)
-		}
-		plate[step][i] = false
-	}
-	return possibilities
+    return total
 }
 
 func main() {
@@ -74,8 +97,7 @@ func main() {
 		fmt.Println("Invalid number !")
 		os.Exit(84)
 	}
-	plate := allocate(nb)
-	solution := nqueen(nb, plate, 0)
+	solution := nqueen(nb, nil, 0, nil, nil, nil)
 	fmt.Println("Solutions :", solution)
 	return
 }
